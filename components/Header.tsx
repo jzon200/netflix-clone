810b63aec9b1c94445fc1ce4814e6ee3ea5f0190
Baus-san <edzonbausa33@gmail.com>
2022-04-19 09:12:07 +0800

    Creating Header Component

---
+++ components/Header.tsx
@@ -0,0 +1,41 @@
+import React, { FC } from 'react'
+import { BellIcon, SearchIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
+
+const Header: FC = () => {
+  return (
+    <header>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+
+        <ul className="hidden space-x-4 md:flex">
+          <li className="header-links">Home</li>
+          <li className="header-links">TV Shows</li>
+          <li className="header-links">Movies</li>
+          <li className="header-links">New & Popular</li>
+          <li className="header-links">My List</li>
+        </ul>
+      </div>
+
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="w-6" />
+        <Link href="/accounts">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export default Header
