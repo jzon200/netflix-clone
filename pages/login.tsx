8c34d3b325e117945e5ca29135e622b97586edfc
Baus-san <edzonbausa33@gmail.com>
2022-04-20 18:04:29 +0800

    Using React Hook Form library

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,37 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+type Inputs = {
+  email: string
+  password: string
+}
 
 const Login = () => {
+  const [login, setLogin] = useState(false)
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>()
+
+  const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
+    if (login) {
+      // await signIn(email, password)
+    } else {
+      // await signUp(email, password)
+    }
+  }
+
+  console.log(watch('email'))
+  console.log(watch('password'))
+
   return (
-    <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
+    <div className="relative flex h-screen w-screen flex-col md:items-center md:justify-center">
       <Head>
         <title>Netflix</title>
         <link
@@ -20,35 +47,47 @@ const Login = () => {
         objectFit="cover"
       />
       {/* Netflix Logo */}
-      <img
-        src="https://rb.gy/ulxxee"
-        className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
-        width={150}
-        height={150}
-      />
+      <div className="absolute left-2 top-1 h-20 w-44 cursor-pointer md:left-8 md:top-4">
+        <Image src="https://rb.gy/ek4j9f" layout="fill" objectFit="contain" />
+      </div>
 
       <form
-        onSubmit={(event) => {
-          event.preventDefault()
-        }}
+        onSubmit={handleSubmit(onSubmit)}
         className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
       >
         <h1 className="text-4xl font-bold">Sign In</h1>
         <div className="space-y-4">
-          <label className="block">
-            <input type="email" placeholder="Email" className="form-control" />
+          <label className="inline-block w-full">
+            <input
+              type="email"
+              placeholder="Email"
+              className="form-control"
+              {...register('email', { required: true })}
+            />
+            {errors.email && (
+              <p className="text-sm  text-orange-500">
+                Please enter a valid email.
+              </p>
+            )}
           </label>
-          <label className="block">
+          <label className="inline-block w-full">
             <input
               type="password"
               placeholder="Password"
               className="form-control"
+              {...register('password', { required: true })}
             />
+            {errors.password && (
+              <p className="text-sm  text-orange-500">
+                Your password must contain between 4 and 60 characters.
+              </p>
+            )}
           </label>
         </div>
 
         <button
           type="submit"
+          onClick={() => setLogin(true)}
           className="w-full rounded bg-[#e50914] px-4 py-3 text-lg font-bold"
         >
           Sign In
@@ -56,7 +95,11 @@ const Login = () => {
 
         <div className="text-[#737373]">
           New to Netflix?{' '}
-          <button type="submit" className="font-medium text-white">
+          <button
+            type="submit"
+            onClick={() => setLogin(false)}
+            className="font-medium text-white"
+          >
             <Link href="#">Sign up now.</Link>
           </button>
         </div>
